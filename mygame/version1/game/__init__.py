70988be0bfac1c3a23dc69b4229d925c1e14fe0f
Wilbur Yang <wilbur.yang@cloudera.com>
2014-09-21 04:27:22 -0700

    Rectangles now move upon click

--- mygame/version1/game/__init__.py
+++ mygame/version1/game/__init__.py
@@ -1,19 +1,75 @@
 import pyglet
 import shapely
 import random
+from objects import *
+from pyglet.window import mouse
 """import pyglet.app
 import pyglet.window
 import pyglet.graphics"""
 
-window = pyglet.window.Window(800, 600)
 
+
+class RoomArranger(object):
+
+  def __init__(self):
+    self.furniture = [Rectangle(10, 30, 20, 30, 3.14), Rectangle(50, 30, 20, 30, -.1)]
+
+  def fitness(self):
+    '''room is a polygon, furniture is a list of polygons'''
+
+    # furniture intersecting
+    intersectionality = 0
+    for i, poly_1 in enumerate(self.furniture):
+      for j, poly_2 in enumerate(self.furniture):
+        if i == j:
+          continue
+        intersectionality += poly_1.intersect(poly_2).area()
+
+    # furniture outside bounds of room
+    outside_room = 0
+    for poly in enumerate(self.furniture):
+      outside_room += poly.difference(outside_room).area()
+
+    return -intersectionality - outside_room * 10
+
+  def generate_random_arrangement(self):
+
+    '''here furniture is a list of things with attributes x, y, width, height, rotation'''
+    for rectangle in self.furniture:
+      for j in range(3):
+        rectangle.mutate()
+
+  def show_arrangement(self):
+    '''here furniture is a list of things with attributes x, y, width, height, rotation'''
+    pyglet.gl.glColor3f(.8,.8,.8)
+
+
+    for rectangle in self.furniture:
+      # redundant with shapely.geometry.Polygon's vertices
+      '''points = []
+      x_axis = [math.cos(rectangle.rotation), math.sin(rectangle.rotation)]
+      y_axis = [-math.sin(rectangle.rotation), math.cos(rectangle.rotation)]
+      for i in range(4):
+        sign1 = (i / 2) * 2 - 1
+        sign2 = ((((i + 1) % 4)) / 2) * 2 - 1
+        points.append((rectangle.x + sign1 * rectangle.width * x_axis[0] / 2 + sign2 * rectangle.height * y_axis[0] / 2, rectangle.y + sign1 * rectangle.width * x_axis[1] / 2 + sign2 * rectangle.height * y_axis[1] / 2 ))'''
+      #poly = shapely.geometry.polygon(zip(points))
+      pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES,
+      [0, 1, 2, 0, 2, 3],
+      ('v2i', [int(coord * 10) for tuple in list(rectangle.exterior.coords[:-1]) for coord in tuple])
+    )
+
+
+ra = RoomArranger()
+
+window = pyglet.window.Window(800, 600)
 
 @window.event
 def on_draw():
   pyglet.gl.glClearColor(240,0,0,255)
   window.clear()
   '''
-  
+
   pyglet.gl.glColor3f(.8,.8,.8)
 
   pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES,
@@ -23,58 +79,13 @@ def on_draw():
              150, 150,
              100, 150))
   )'''
+  ra.show_arrangement()
 
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+  if button == mouse.LEFT:
+    ra.generate_random_arrangement()
+    ra.show_arrangement()
 
-def fitness(room, furniture):
-  '''room is a polygon, furniture is a list of polygons'''
-  
-  # furniture intersecting
-  intersectionality = 0
-  for i, poly_1 in enumerate(furniture):
-    for j, poly_2 in enumerate(furniture):
-      if i == j:
-        continue
-      intersectionality += poly_1.intersect(poly_2).area()
-
-  # furniture outside bounds of room
-  outside_room = 0
-  for poly in enumerate(furniture):
-    outside_room += poly.difference(outside_room).area()
-
-  return -intersectionality - outside_room*10
-
-def mutate(rectangle):
-  rectangle.x += random.uniform(-10, 10)
-  rectangle.y += random.uniform(-10, 10)
-  rectangle.rotation += random.uniform(-0.1, 0.1)
-
-def generate_random_arrangement(furniture):
-  '''here furniture is a list of things with attributes x, y, width, height, rotation'''
-  for rectangle in furniture:
-    for i in range(10):
-      mutate(rectangle)
-
-def show_arrangement(furniture):
-  '''here furniture is a list of things with attributes x, y, width, height, rotation'''
-  pyglet.gl.glColor3f(.8,.8,.8)
-
-  
-  for rectangle in furniture:
-    points = []
-    x_axis = [math.cos(rectangle.angle), math.sin(rectangle.angle)]
-    y_axis = [-math.sin(rectangle.angle), math.cos(rectangle.angle)]
-    for i in range(4):
-      sign1 = (i / 2) * 2 - 1
-      sign2 = ((((i + 1) % 4)) / 2) * 2 - 1
-      points.append((rectangle.x + sign1 * rectangle.width * x_axis[0] / 2 + sign2 * rectangle.height * y_axis[0] / 2, rectangle.y + sign1 * rectangle.width * x_axis[1] / 2 + sign2 * rectangle.height * y_axis[1] / 2 ))
-    #poly = shapely.geometry.polygon(zip(points))
-    pyglet.graphics.draw_indexed(4, pyglet.gl.GL_TRIANGLES,
-    [0, 1, 2, 0, 2, 3],
-    ('v2i', (points[0][0], points[0][1],
-             points[1][0], points[1][1],
-             points[2][0], points[2][1],
-             points[3][0], points[3][1],))
-  )
-
-
-pyglet.app.run()
+if __name__ == '__main__':
+  pyglet.app.run()
---
+++ mygame/version1/game/objects.py
@@ -0,0 +1,58 @@
+import shapely
+import math
+import random
+from shapely.geometry import Polygon
+
+
+def rotate(t, rotation):
+  return (t[0] * math.cos(rotation) - t[1] * math.sin(rotation), t[0] * math.sin(rotation) + t[1] * math.cos(rotation))
+
+mx = [1, -1, -1, 1]
+my = [1, 1, -1, -1]
+
+class Rectangle(Polygon):
+
+  def __init__(self, x, y, width, height, rotation):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.rotation = rotation
+
+    w2 = width / 2.
+    h2 = height / 2.
+    l = []
+    for k in range(4):
+      l.append(rotate((mx[k] * w2, my[k] * h2), rotation))
+
+    l = [(a[0] + x, a[1] + y) for a in l]
+    Polygon.__init__(self, l)
+
+  def mutate(self):
+    self.x += random.uniform(-10, 10)
+    self.y += random.uniform(-10, 10)
+    self.rotation += random.uniform(-0.1, 0.1)
+    self.__init__(self.x, self.y, self.width, self.height, self.rotation)
+
+  def setx(self, x):
+    self.x = x
+
+  def isMovable(self):
+    # to be implemented in subclasses
+    raise NotImplementedError
+
+class MovableRectangle(Rectangle):
+  def __init__(self):
+    super(self)
+
+  def isMovable(self):
+    return True
+
+class ImmovableRectangle(Rectangle):
+  def __init__(self):
+    super(self)
+
+  def isMovable(self):
+    return False
+
+
